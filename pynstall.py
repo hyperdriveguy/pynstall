379ab91ea529bb258aaf6d69b0d5f0940ec2f294
hyperdriveguy <hyperdriveguy@users.noreply.github.com>
2015-03-18 21:32:43 -0700

    Omitted the gui

--- pynstall.py
+++ pynstall.py
@@ -1,34 +1,41 @@
 #!/usr/bin/python
 import apt
 import os
-import tkinter as tk
-tkinter.Tcl(screenName="Pynstall", baseName=None, className='Tk', useTk=0)
+import sys
 
 cache = apt.cache.Cache()
 cache.update()
+cache.open()
 
-master = Tk()
-#Initailize Package
-pkg_name = Entry(pkgn)
-pkg_name.pack()
-pkg_name.focus_set()
-pkg_to_install = pkg_name.get()
-pkg = cache[pkg_name]
-
-def install():
-  if pkg.is_installed:
-    tk.messagebox("Package is already installed.")
+exit = 0
+print("Make sure you are root.")
+while exit = 0:
+  command = input("What would you like to do? Type help for options.")
+  if command == "help":
+    print("Commands are: install(Installs packages) remove(Removes packages) update(Updates packages) exit(Exit this program) help(display this screen")
+  elif command == "install":
+    pkg_name = input("Type the name of the package you want to install)
+    pkg = cache[pkg_name]
+    if pkg.is_installed:
+      print "{pkg_name} already installed".format(pkg_name=pkg_name)
   else:
-    pkg.mark_install()
-  try:
-        cache.commit()
-  tk.messagebox("Package is now installed.")
-def remove():
-  os.system("apt-get remove " + pkg)
-  tk.messagebox("Package removed.")
-install_pkg = Button(master, text="Install", width=15, command=install)
-install_pkg.pack()
+      pkg.mark_install()
 
-uninstall_pkg = Button(master, text="Remove", width=15, command=remove)
-uninstall_pkg.pack()
-mainloop()
+      try:
+          cache.commit()
+      except Exception, arg:
+          print >> sys.stderr, "Sorry, package installation failed [{err}]"(err=str(arg))
+  elif command == "remove":
+    package = input("What package do you want to remove?")
+    os.system("apt-get remove " + package)
+    print(package.capitalize + " was removed.")
+  elif command == "update":
+   os.system("apt-get update")
+   os.system("apt-get upgrade")
+   print("System updated.")
+  elif command == "exit":
+    print("exiting...")
+   os.system("clear")
+    exit = 1
+  else:
+   print("Command not recognized. Type 'help' for a list of commands.")
