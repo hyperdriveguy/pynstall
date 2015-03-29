e2ddb3ee618e4ca52d39a62db91795b741e07123
hyperdriveguy <hyperdriveguy@users.noreply.github.com>
2015-03-28 17:19:22 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,3 +2,6 @@
 echo "PYNSTALL INSTALLER"
 echo "Make 
 read -e -p "Enter the name of your home directory: " home
+
+mv /home/$home/Downloads/pynstall/pynstall.py /home/$home/.pynstall/pynstall.py
+mv /home/$home/Downloads/pynstall/pynstall.desktop /home/$home/Desktop/pynstall.desktop
