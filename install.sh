fd4b5dd6340ef74e2eeb34b4a031959628c6c6fd
hyperdriveguy <hyperdriveguy@users.noreply.github.com>
2015-04-23 08:48:43 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,8 @@
 #!/bin/bash/sh
 echo "PYNSTALL INSTALLER"
-echo "Make 
+echo "Make sure the Pynstall folder is in your Downloads directory."
 read -e -p "Enter the name of your home directory: " home
 
-mv /home/$home/Downloads/pynstall/pynstall.py /home/$home/.pynstall/pynstall.py
+mv /home/$home/Downloads/pynstall/pynstall.py /usr/bin/pynstall.py
 mv /home/$home/Downloads/pynstall/pynstall.desktop /home/$home/Desktop/pynstall.desktop
+
