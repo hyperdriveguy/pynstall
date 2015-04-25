25158b7008ca4c2c91895ef2ed661606c7e25d1f
hyperdriveguy <hyperdriveguy@users.noreply.github.com>
2015-04-24 21:08:33 -0700

    Eliminate asking about home

--- install.sh
+++ install.sh
@@ -8,15 +8,14 @@ sudo apt-get install --force-yes python3 python3-apt
 clear
 read -e -p "Enter the name of the directory the Pynstall folder is in. For example: /home/derp/Downloads: " pynstall
 clear
-#Ask for the name of the home directory
-read -e -p "Enter the name of your home directory: " home
-clear
 #Move files
 sudo mv $pynstall/pynstall.py /usr/bin/pynstall.py
 sudo mv $pynstall/pynstall/pynstall /usr/bin/pynstall
 sudo mv $pynstall/pynstall/python1.svg /usr/share/python1.svg
+mv $pynstall/pynstall/pynstall.desktop $HOME/Desktop/pynstall.desktop
 #Delete files
-mv $pynstall/pynstall/install.sh /home/$home/install.sh
-rm $pynstall/pynstall
+mv $pynstall/pynstall/install.sh $HOME/install.sh
+rm $pynstall/pynstall/README.md
+rm $pynstall/pynstall/.gitignore
 echo "Installer finished!"
 exit
