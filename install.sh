02ec0b9ae07e63d7220668a6f59c686ca686fad1
hyperdriveguy <hyperdriveguy@users.noreply.github.com>
2015-07-16 22:37:53 -0700

    Made installer more complete
    Added [sudo mv $pynstall/pynstall/LICENSE /usr/share/pynstall/LICENSE] on line 18.
    Added [rm -rf $pynstall/pynstall] on line 22.

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ sudo apt-get install --force-yes python3 python3-apt
 clear
 read -e -p "Enter the name of the directory the Pynstall folder is in. For example: /home/derp/Downloads: " pynstall
 clear
-#Make Directory
+#Make directory
 sudo mkdir /usr/share/pynstall
 #Move files
 sudo mv $pynstall/pynstall.py /usr/bin/pynstall.py
@@ -15,8 +15,10 @@ sudo mv $pynstall/pynstall/pynstall /usr/bin/pynstall
 sudo mv $pynstall/pynstall/python1.svg /usr/share/pynstall/python1.svg
 sudo mv $pynstall/pynstall/README.md /usr/share/pynstall/README.md
 sudo mv $pynstall/pynstall/.gitignore /usr/share/pynstall/.gitignore
+sudo mv $pynstall/pynstall/LICENSE /usr/share/pynstall/LICENSE
 mv $pynstall/pynstall/pynstall.desktop $HOME/Desktop/pynstall.desktop
-#Delete files
+#Delete installer and pynstall directory
 rm $pynstall/pynstall/install.sh
+rm -rf $pynstall/pynstall
 echo "Installer finished!"
 exit
